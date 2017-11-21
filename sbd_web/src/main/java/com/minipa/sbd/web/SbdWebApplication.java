f6a6e3720eb04a4bc9ba22525795a85add34e3e1
miniPa <eric.fourpackcn@outlook.com>
2017-11-21 21:57:27 +0800

    web

---
+++ sbd_web/src/main/java/com/minipa/sbd/web/SbdWebApplication.java
@@ -0,0 +1,12 @@
+package com.minipa.sbd.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdWebApplication.class, args);
+	}
+}
---
+++ sbd_web/src/main/java/com/minipa/sbd/web/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.minipa.sbd.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController:
+ * author: Chengjs, version:1.0.0, 2017-11-21
+ * @RestController 意味controller里面的方法都以json格式输出，不用再写什么jackjson配置
+ */
+@RestController
+public class HelloController {
+
+  @RequestMapping("/hello") // http://localhost:8080/hello
+  public String hello() {
+    return "Hello World";
+  }
+
+}
