32d89c8921076e3cecff32858946e8f7a7564232
miniPa <eric.fourpackcn@outlook.com>
2017-11-21 23:15:40 +0800

    web

---
+++ sbd_redis/src/main/java/com/minipa/sbd/redis/SbdRedisApplication.java
@@ -0,0 +1,16 @@
+package com.minipa.sbd.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  Redis: 使用最广泛的内存数据存储 支持的数据结构丰富, 支持数据持久化
+ *  Redis还提供一些类数据库的特性，比如事务，HA，主从库
+ */
+@SpringBootApplication
+public class SbdRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdRedisApplication.class, args);
+	}
+}
---
+++ sbd_web/src/test/java/com/minipa/sbd/web/controller/HelloControllerTest.java
@@ -0,0 +1,40 @@
+package com.minipa.sbd.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * HelloControllerTest:
+ * mockmvc编写简单http请求来测试
+ * 利用MockMvcResultHandlers.print()打印结果
+ *
+ * author: Chengjs, version:1.0.0, 2017-11-21
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloControllerTest {
+  private MockMvc mvc;
+
+  @Before
+  public void setUp() throws Exception {
+    mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+  }
+
+  @Test
+  public void hello() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string(equalTo("Hello World")));
+  }
+
+}
