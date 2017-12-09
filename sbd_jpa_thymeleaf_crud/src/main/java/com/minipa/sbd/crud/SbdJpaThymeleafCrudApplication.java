7c16a9ff50563b2e5040d02ded24ddeb14b9963e
miniPa <eric.fourpackcn@outlook.com>
2017-12-09 13:34:38 +0800

    sbd_jpa_thymeleaf

--- sbd_jpa_thymeleaf_crud/src/main/java/com/minipa/sbd/crud/SbdJpaThymeleafCrudApplication.java
+++ sbd_jpa_thymeleaf_crud/src/main/java/com/minipa/sbd/crud/SbdJpaThymeleafCrudApplication.java
@@ -2,11 +2,25 @@ package com.minipa.sbd.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SbdJpaThymeleafCrudApplication {
+public class SbdJpaThymeleafCrudApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbdJpaThymeleafCrudApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SbdJpaThymeleafCrudApplication.class, args);
+  }
+
+  /**
+   * 添加Servlet支持
+   *
+   * @param application
+   * @return
+   */
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SbdJpaThymeleafCrudApplication.class);
+  }
+
 }
